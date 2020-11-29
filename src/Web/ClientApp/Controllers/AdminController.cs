3dd5ce6f0083c8c82f6236af211acbde682c7b90
Daniel Madrala <dmadrala@wp.pl>
2020-11-29 12:24:57 +0100

    added admin functionality

---
+++ src/Web/ClientApp/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ClientApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> ClaimsSummary()
+        {
+            return View();
+        }
+    }
+}
